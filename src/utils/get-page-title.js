c1fba62b8abb9192fb086a245e6f7ff60b5e6707
faligroundhog <1360783249@qq.com>
2023-07-04 15:53:17 +0800

    第一次提交

---
+++ src/utils/get-page-title.js
@@ -0,0 +1,11 @@
+import defaultSettings from '@/settings'
+
+const title = defaultSettings.title || 'Vue Admin Template'
+
+export default function getPageTitle(pageTitle) {
+  if (pageTitle) {
+    console.log(title);
+    return `${pageTitle} - ${title}`
+  }
+  return `${title}`
+}
